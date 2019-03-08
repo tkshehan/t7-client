ab27b39f73c00a393aa8f7530911d706f331511d
Thomas Shehan <thomas.shehan88@gmail.com>
2019-03-07 20:42:08 -0500

    add saving and initial data

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Server Repo at https://github.com/tkshehan/t7-server
 
 # ToDo
 - [x] Add throws for characters with seperate data
+- [ ] Store data for offline functionality
 - [ ] Style for Mobile
 - [ ] Colorize on block -10 and worse [-10, -12, and -15 respectively]
 - [ ] Replace Notes with Icons
 - [ ] Add initial data to mitigate loading times on first use
-- [ ] Store data for offline functionality
 - [ ] Port to Android
 
 # ToDoServerSide
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CharacterSelect from './CharacterSelect';
 import MoveList from './MoveArray';
+import initialData from '../initialData';
 
 class App extends Component {
   constructor(props) {
@@ -11,14 +12,30 @@ class App extends Component {
       characters: [],
       movelists: {},
       selected: '',
+      KEY: 'FRAMEDATA',
     };
   }
 
   componentDidMount() {
-    this.fetchData();
+    this.loadData();
+    this.fetchAndSaveData();
   }
 
-  fetchData = () => {
+  loadData = () => {
+    const result = localStorage.getItem(this.state.KEY) || JSON.stringify(initialData);
+    const data = JSON.parse(result);
+    const characters = Object.keys(data);
+    this.setState({
+      characters: characters,
+      movelists: data,
+    });
+  }
+
+  saveData = (data) => {
+    localStorage.setItem(this.state.KEY, JSON.stringify(data));
+  }
+
+  fetchAndSaveData = () => {
     fetch("https://t7frames-server.herokuapp.com/frame-data")
       .then(res => {
         return res.json()
@@ -31,6 +48,7 @@ class App extends Component {
             characters: characters,
             movelists: result,
           });
+          this.saveData(result);
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
