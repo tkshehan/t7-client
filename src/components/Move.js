063a0b158bafe8e57226355e2fa879817975ad4d
Thomas Shehan <thomas.shehan88@gmail.com>
2019-05-15 15:00:14 -0400

    colorize punishable moves

--- src/components/Move.js
+++ src/components/Move.js
@@ -3,11 +3,30 @@ import React from 'react'
 class Move extends React.Component {
   render() {
     let properties = Object.keys(this.props.properties);
+    let onBlock = parseInt(this.props.properties.Block);
+    let punish = '';
+    if (onBlock <= -15) {
+      punish = 'i15';
+    } else if (onBlock <= -12) {
+      punish = 'i12';
+    } else if (onBlock <= -10) {
+      punish = 'i10';
+    }
+
+
     return (
       <tr>
         {
           properties.map((prop, i) => {
-            return (<td key={i}>{this.props.properties[prop]}</td>);
+            if (i === 4) {
+              return (<td className={punish} key={i}>
+                {this.props.properties[prop]}
+              </td>);
+            } else {
+              return (<td key={i}>
+                {this.props.properties[prop]}
+              </td>);
+            }
           })
         }
       </tr>
